473220263bfaf2ba8d39a10319a8fe83e8278b28
ADHIL <adhilbinmujeeb@gmail.com>
2025-04-17 23:11:16 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+Business Insights Hub
+A comprehensive business valuation and assessment platform.
+"""
+
+from . import core
+from . import models
+from . import services
+
+__version__ = '0.1.0'
+__author__ = 'Your Name'
+
+__all__ = ['core', 'models', 'services']
